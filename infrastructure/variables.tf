63a44ea24fd226a4daa806d6bef8e20d231751d3
edilson-lobo <edilsonmlobo21@gmail.com>
2022-05-07 08:29:49 -0300

    Sobe infra airflow

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,3 +5,15 @@ variable "aws_region" {
 variable "lambda_function_name" {
   default = "IGTIexecutaEMR"
 }
+
+variable "key_pair_name" {
+  default = "elobo-igti-teste"
+}
+
+variable "airflow-subnet_id" {
+  default = "subnet-06f8d51532672a2d9"
+}
+
+variable "vpc_id" {
+  default = "vpc-06872304132e932f7"
+}
